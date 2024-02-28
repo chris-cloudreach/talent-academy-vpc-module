fa55583eb07475c2fdce18d38755a6ced8cb00b1
Christopher Ogbunuzor <ogbunuzorchristopher@yahoo.com>
2024-02-28 00:08:35 +0000

    Add default to ec2created variable

--- ec2.tf
+++ ec2.tf
@@ -17,14 +17,14 @@ resource "aws_instance" "packer_private_ec2" {
   count = var.ec2created == true ? 1:0
   ami                         = "ami-052c9ea013e6e3567" #would need to use data to fetch latest AMI
   instance_type               = "t2.micro"
-  associate_public_ip_address = true
+  associate_public_ip_address = false
   key_name                    = "ta-lab-key"
   subnet_id                   = aws_subnet.private_a.id
   vpc_security_group_ids      = [aws_security_group.packer-security-group.id]
   user_data = file("packerInstall.sh")
 
   tags = {
-    Name = "Packer-Bastion"
+    Name = "Packer-Private"
   }
 }
 
